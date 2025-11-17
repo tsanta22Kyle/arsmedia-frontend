a1bb73141fecbe0e65daa9fd95ba1cf4384675ab
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-11-17 22:05:27 +0300

    chore : update input snapshot in tests

--- __tests__/unit/Input.test.tsx
+++ __tests__/unit/Input.test.tsx
@@ -8,12 +8,11 @@ afterEach(()=>{cleanup()})
 
 describe('<Input>',()=>{
     it("should render same input with same style",()=>{
-      const {container} =  render(<Input></Input>)
+      const {container} =  render(<Input placeholder={""}></Input>)
       expect(container.firstChild).toMatchInlineSnapshot(`
 <input
-  class="border border-[#8080805a] focus:outline-2 outline-primary  rounded-xl py-3 px-2"
-  id="username"
-  placeholder="user_name"
+  class="undefined border border-[#8080805a] focus:outline-2 outline-primary  rounded-xl py-3 px-2"
+  placeholder=""
   type="text"
 />
 `)
