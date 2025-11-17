9c3f5e34fc25bbd3f0b8a0c45f319278b1f9d89e
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-11-17 21:52:27 +0300

    fix : remove label on button demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <Button label="click" ></Button>
     </div>
   );
 }
