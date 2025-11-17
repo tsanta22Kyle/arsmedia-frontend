9b340e67b71ce3a3cb31ecb51272de934d1cb090
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-11-17 19:07:52 +0300

    feat :  add input style rendering tests

---
+++ __tests__/unit/Input.test.tsx
@@ -0,0 +1,23 @@
+import Input from "@/src/app/components/Input"
+import { cleanup, render, screen } from "@testing-library/react"
+import { userEvent} from "@testing-library/user-event"
+
+
+// pour eviter les effets de bords / anomalies lors de changements de data systématiques
+afterEach(()=>{cleanup()})
+
+describe('<Input>',()=>{
+    it("should render same input with same style",()=>{
+      const {container} =  render(<Input></Input>)
+      expect(container.firstChild).toMatchInlineSnapshot(`
+<input
+  class="border border-[#8080805a] focus:outline-2 outline-primary  rounded-xl py-3 px-2"
+  id="username"
+  placeholder="user_name"
+  type="text"
+/>
+`)
+    })
+
+  
+})
---
+++ src/app/components/Input.tsx
@@ -0,0 +1,7 @@
+
+export default function Input(){
+    return(
+                            <input type="text" id="username" className="border border-[#8080805a] focus:outline-2 outline-primary  rounded-xl py-3 px-2"  placeholder="user_name" />
+
+    )
+}
