ec6f58b389d0d70eda4142c66af88286661cb333
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-11-27 23:36:33 +0300

    feat : add dynamic inputs on signup with react-hook-form

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -5,9 +5,25 @@ import PhonePrefixDropdown from "../../components/PhonePrefixDropdown";
 import Image from "next/image";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  pseudo : string,
+  email: string,
+  password : string,
+  phoneNumber : string,
+}
 
 export default function SignUp() {
   const [prefixValue, setPrefixValue] = useState("UK");
+  const {register , handleSubmit,formState : {errors}} = useForm<Inputs> ();
+
+
+  console.log(errors.email);
+  const onSubmit = (data : Inputs)=>{
+    console.log(data);
+    
+  }
 
   function handlePrefixChange(code: string) {
     setPrefixValue(code);
@@ -28,7 +44,7 @@ export default function SignUp() {
           </p>
         </div>
 
-        <form className=" flex flex-col p-25 max-lg:p-5 rounded-2xl relative bg-transparent lg:w-1/2  gap-5">
+        <form onSubmit={handleSubmit(onSubmit)} className=" flex flex-col p-25 max-lg:p-5 rounded-2xl relative bg-transparent lg:w-1/2  gap-5">
           <header className="flex flex-col justify-center items-center gap-3 w-full">
             {/* <Image alt="logo arsmedia" className="w-30" src={"/assets/logo-light.png"} width={0} height={0} unoptimized ></Image> */}
             <h1 className="text-4xl font-semibold">Sign up</h1>
@@ -41,19 +57,28 @@ export default function SignUp() {
               <label htmlFor="username" className="px-2">
                 username
               </label>
-              <Input type="text" id="username" placeholder="user_name" />
+              <Input {...register("pseudo",{required : "user name is required"})}  type="text" id="username" placeholder="user_name" />
+              {
+                errors.pseudo?.type =="required" && <label className="text-xs text-red-500" >user name is required</label>
+              }
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="username" className="px-2">
                 email
               </label>
-              <Input type="text" id="username" placeholder="example@mail.xyz" />
+              <Input {...register("email",{required : "email is required"})} type="text" id="username" placeholder="example@mail.xyz" />
+               {
+                errors.email?.type =="required" && <label className="text-xs text-red-500" >email is required</label>
+              }
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="username" className="px-2">
                 password
               </label>
-              <Input type="password" id="username" placeholder="user_name" />
+              <Input {...register('password',{required : "password is required"})} type="password" id="username" placeholder="user_name" />
+               {
+                errors.password?.type =="required" && <label className="text-xs text-red-500" >password is required</label>
+              }
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="username" className="px-2">
@@ -65,12 +90,17 @@ export default function SignUp() {
                   onChange={handlePrefixChange}
                 ></PhonePrefixDropdown>
                 <Input
-                  type="password"
+                  {...register("phoneNumber",{required : "phone number is required"})}
+                  type="text"
                   id="username"
                   className=" w-[75%] border-l rounded-l-none border-t-0 border-b-0 rounded-r-xl border-[#8080805a] py-3 px-2 focus:outline-2 outline-primary "
                   placeholder="a phone number"
                 />
+                
               </div>
+               {
+                errors.phoneNumber?.type =="required" && <label className="text-xs text-red-500" >phone number is required</label>
+              }
             </div>
             <div className="w-full flex items-center justify-center ">
               <Button className="px-20">Sign up</Button>
--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,14 +1,14 @@
-import { HTMLInputTypeAttribute } from "react";
+import { HTMLInputTypeAttribute, InputHTMLAttributes } from "react";
 
-interface Props{
+type Props ={
     type?: HTMLInputTypeAttribute | undefined;
     id?:string | undefined;
     className?:string;
     placeholder:string;
-}
-export default function Input({type ="text",id , className , placeholder}:Props){
+} & InputHTMLAttributes<HTMLInputElement>
+export default function Input({type ="text",id , className , placeholder , ...rest}:Props){
     return(
-                            <input type={type} id={id} className={`${className} border border-[#8080805a] focus:outline-2 outline-primary  rounded-xl py-3 px-2`}  placeholder={placeholder} />
+                            <input type={type} {...rest} id={id} className={`${className} border border-[#8080805a] focus:outline-2 outline-primary  rounded-xl py-3 px-2`}  placeholder={placeholder} />
 
     )
 }
